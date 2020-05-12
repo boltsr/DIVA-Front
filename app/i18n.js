75656c029f38850e62572bfd4f804175d68b421c
joysk <skrullweb.dev@gmail.com>
2020-05-12 11:20:53 +1000

    fix i18n localization

--- app/i18n.js
+++ app/i18n.js
@@ -21,20 +21,20 @@ const appLocales = [
   'en',
 ];
 
-// const formatTranslationMessages = (locale, messages) => {
-//   const defaultFormattedMessages =
-//     locale !== DEFAULT_LOCALE
-//       ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
-//       : {};
-//   const flattenFormattedMessages = (formattedMessages, key) => {
-//     const formattedMessage =
-//       !messages[key] && locale !== DEFAULT_LOCALE
-//         ? defaultFormattedMessages[key]
-//         : messages[key];
-//     return Object.assign(formattedMessages, { [key]: formattedMessage });
-//   };
-//   return Object.keys(messages).reduce(flattenFormattedMessages, {});
-// };
+const formatTranslationMessages = (locale, messages) => {
+  const defaultFormattedMessages =
+    locale !== DEFAULT_LOCALE
+      ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
+      : {};
+  const flattenFormattedMessages = (formattedMessages, key) => {
+    const formattedMessage =
+      !messages[key] && locale !== DEFAULT_LOCALE
+        ? defaultFormattedMessages[key]
+        : messages[key];
+    return Object.assign(formattedMessages, { [key]: formattedMessage });
+  };
+  return Object.keys(messages).reduce(flattenFormattedMessages, {});
+};
 
 const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
